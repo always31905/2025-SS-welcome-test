2c58d9f64d7cbc471356333d790a376889805802
always31905 <bmw98334188@gmail.com>
2025-03-02 21:03:27 +0900

    Update g1.java

--- java_test/g1.java
+++ java_test/g1.java
@@ -1,11 +1,47 @@
 package java_test;
+import java.util.Scanner;
 
-// 이부분에 class 선언 해도 괜찮습니다
+class Fan {
+    private String model;
+    private int price;
 
+    public Fan() {}
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
 public class g1 {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner sc = new Scanner(System.in);
+        Fan[] fans = new Fan[3];
+
+        for (int i = 0; i < 3; i++) {
+            fans[i] = new Fan();
+            fans[i].setModel(sc.next());
+            fans[i].setPrice(sc.nextInt());
+        }
+
+        sc.close();
+
+        Fan cheapestFan = fans[0];
+        for (int i = 1; i < 3; i++) {
+            if (fans[i].getPrice() < cheapestFan.getPrice()) {
+                cheapestFan = fans[i];
+            }
+        }
+
+        System.out.println(cheapestFan.getModel());
 
     }
 
