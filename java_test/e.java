d59908ef23e66251ab2d39c10086c3d976b5c546
always31905 <bmw98334188@gmail.com>
2025-03-02 20:58:52 +0900

    Update e.java

--- java_test/e.java
+++ java_test/e.java
@@ -1,10 +1,27 @@
 package java_test;
-
+import java.util.Scanner;
 public class e {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();  
+        int month = sc.nextInt();
 
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                System.out.println(31);
+                return;
+            case 4: case 6: case 9: case 11:
+                System.out.println(30);
+                return;
+        }
+
+        if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
+            System.out.println(28);
+        } else {
+            System.out.println(29);
+        }
+        sc.close();
     }
 
 }
