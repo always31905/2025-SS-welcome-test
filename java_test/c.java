2aaddf57a9d8f8e0650138b70b7c710c9c0738f1
always31905 <bmw98334188@gmail.com>
2025-03-02 20:55:53 +0900

    Update c.java

--- java_test/c.java
+++ java_test/c.java
@@ -1,10 +1,18 @@
 package java_test;
-
+import java.util.Scanner;
 public class c {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt(); 
+        int height = sc.nextInt(); 
 
+        if (age >= 14 || height >= 155) {
+            System.out.println("X");
+            return; 
+        }
+        System.out.println("O");
+        sc.close();
     }
 
 }
