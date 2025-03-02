9fb1aed231ec222251c17cab3c7556da669aa3d6
always31905 <bmw98334188@gmail.com>
2025-03-02 20:54:31 +0900

    Update b.java

--- java_test/b.java
+++ java_test/b.java
@@ -1,9 +1,19 @@
 package java_test;
-
+import java.util.Scanner;
 public class b {
     public static void main(String[] args) {
-
-        // 이 부분에 code를 작성해주세요!
+        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(); 
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+                System.out.println("O");
+                return;
+            }
+        }
+        System.out.println("X");
+        sc.close();
 
     }
 
